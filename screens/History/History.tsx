9492bab25d6b718fea2b0efbeb559c0d2086409c
Mattan1999 <mattias470@gmail.com>
2023-07-12 19:16:45 +0200

    updates

--- screens/History/History.tsx
+++ screens/History/History.tsx
@@ -1,11 +1,45 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+
+import { Title } from "react-native-paper";
+
+import workoutStore from "../../store/workoutStore/workoutStore";
+import { Observer } from "mobx-react";
+
+import { useTheme } from "@react-navigation/native";
 
 const History = () => {
+  const theme = useTheme();
   return (
-    <View>
-      <Text>History</Text>
-    </View>
+    <Observer>
+      {() => (
+        <FlatList
+          contentContainerStyle={{
+            marginHorizontal: 10,
+            marginVertical: 10,
+            borderRadius: 10,
+          }}
+          ItemSeparatorComponent={() => <View style={{ height: 5 }} />}
+          data={workoutStore.workoutHistory.slice()}
+          numColumns={2}
+          key={2}
+          renderItem={({ item, index }) => (
+            <Title
+              style={{
+                backgroundColor: theme.colors.card,
+                width: "100%",
+                paddingVertical: 12,
+                paddingHorizontal: 12,
+                color: theme.colors.text,
+                borderRadius: 10,
+              }}
+            >
+              {item.date}
+            </Title>
+          )}
+        />
+      )}
+    </Observer>
   );
 };
 
